f0f7f8d777a4ef17c09e0677b0ca0ebc7d5038a9
Crobrose <Crobrose.noreply.github.com>
2021-03-25 20:48:35 +0700

    Add all object

---
+++ obj/src/ChessDv/ChessDv.d
@@ -0,0 +1,3 @@
+obj\src\ChessDv\ChessDv.o: src\ChessDv\ChessDv.cpp \
+ src/libChessDv\ChessDvHelper.h
+src/libChessDv\ChessDvHelper.h:
---
+++ obj/src/ChessDv/ChessDvHelper.d
@@ -0,0 +1,3 @@
+obj\src\ChessDv\ChessDvHelper.o: src\libChessDv\ChessDvHelper.cpp \
+ src\libChessDv\ChessDvHelper.h
+src\libChessDv\ChessDvHelper.h:
