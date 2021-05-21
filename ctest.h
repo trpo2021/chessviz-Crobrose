a6596ad7d920980e9452ca891a29d1b28431721b
Crobrose <Crobrose.noreply.github.com>
2021-05-21 18:56:33 +0700

    ctest 7

--- ctest.h
+++ ctest.h
@@ -284,12 +284,12 @@ void assert_dbl_far(
 
 #include <setjmp.h>
 #include <stdarg.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/time.h>
 #include <unistd.h>
-#include <stdint.h>
-#include <stdlib.h>
 #include <wchar.h>
 
 static size_t ctest_errorsize;
